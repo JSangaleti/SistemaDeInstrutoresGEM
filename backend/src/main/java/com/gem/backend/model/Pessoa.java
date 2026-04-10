d76c46597125a0a4d1c609b8cc892a97e0111cb0
Leonardo <110777414+leonardofakeBR@users.noreply.github.com>
2026-04-10 15:24:56 -0300

    fix: adiciona codigo esquecido na entidade Pessoa
    Adiciona os atributos e relacionamentos que faltaram no commit anterior

--- backend/src/main/java/com/gem/backend/model/Pessoa.java
+++ backend/src/main/java/com/gem/backend/model/Pessoa.java
@@ -8,6 +8,32 @@ package com.gem.backend.model;
  *
  * @author leonardo
  */
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "pessoas")
 public class Pessoa {
-    
+
+    @Id
+    @Column(length = 11, nullable = false)
+    private String cpf;
+
+    @Column(length = 64, nullable = false)
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(name = "id_comum", referencedColumnName = "id")
+    private Comum comum;
+
+    public Pessoa() {}
+
+    public String getCpf() { return cpf; }
+    public void setCpf(String cpf) { this.cpf = cpf; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public Comum getComum() { return comum; }
+    public void setComum(Comum comum) { this.comum = comum; }
 }
